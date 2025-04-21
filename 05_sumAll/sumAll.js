0a55d149c6b9433ad194c710a07c728718e47aa3
Aaron Saucedo <ajsm.0596@gmail.com>
2025-04-21 11:30:16 -0600

    feat:finish 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (rangeStart, rangeEnd) {
+    let sum = 0;
 
+    if (rangeStart < 0 || rangeEnd < 0 || !Number.isInteger(rangeStart) || !Number.isInteger(rangeEnd)) {
+        return 'ERROR'
+    }
+
+    if (rangeStart > rangeEnd) {
+        [rangeStart, rangeEnd] = [rangeEnd, rangeStart];
+    }
+    for (let i = rangeStart; i <= rangeEnd; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
