08cae161dd856a0f8da955e3407d23dcebbea877
Aaron Saucedo <ajsm.0596@gmail.com>
2025-04-21 11:12:32 -0600

    feat: Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...value) {
+    let valuesToRemove = [...value];
+    for(let i = array.length - 1; i >= 0; i--) {
+    		console.log(array[i]);
+        if(valuesToRemove.includes(array[i])) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
