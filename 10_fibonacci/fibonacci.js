cadb6f94eb4572fdf35f3d337f0af95fb798558a
Aaron Saucedo <ajsm.0596@gmail.com>
2025-04-28 09:44:42 -0600

    feat: exercise 3

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+// Fibonacci number => sum of previous 2 numbers;
 
+const fibonacci = function(number) {
+    let _number = parseInt(number);
+
+    if(!_number) {
+        return 0;
+    } else if (_number < 0) {
+        return 'OOPS';
+    }
+
+    let firstNumber = 1;
+    let secondNumber = 0;
+
+    for(let i = 2; i <= _number; i++) {
+        let total = firstNumber + secondNumber;
+        secondNumber = firstNumber
+        firstNumber = total;
+    }
+
+    return firstNumber;
 };
 
 // Do not edit below this line
