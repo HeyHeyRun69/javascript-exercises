7e0696526e1e3f82ba7ac4b34a0d1591e85bb954
Aaron Saucedo <ajsm.0596@gmail.com>
2025-04-28 10:34:05 -0600

    feat: last exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((accumulator, currentPerson) => {
+        if(!accumulator.yearOfDeath) {
+            accumulator.yearOfDeath = new Date().getFullYear();
+        }
+        if(!currentPerson.yearOfDeath) {
+            currentPerson.yearOfDeath = new Date().getFullYear();
+        }
+        
+        let oldestAge = accumulator.yearOfDeath - accumulator.yearOfBirth;
+        let currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
 
+        return oldestAge > currentAge ? accumulator : currentPerson;
+    })
 };
 
 // Do not edit below this line
