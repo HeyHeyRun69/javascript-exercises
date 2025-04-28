64d8f4699171c6f5a33ed886364b526f0beefe0a
Aaron Saucedo <ajsm.0596@gmail.com>
2025-04-28 09:55:28 -0600

    feat: exercise 4

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    return books.reduce((accumulator, currentValue) => {
+        accumulator.push(currentValue.title)
+        return accumulator;
+    }, [])
 };
 
 // Do not edit below this line
