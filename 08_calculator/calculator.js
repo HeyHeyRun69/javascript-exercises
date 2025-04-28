921d891dd6d75c6bd7c6ad652ca0ee44f755f20c
Aaron Saucedo <ajsm.0596@gmail.com>
2025-04-28 09:06:21 -0600

    feat: exercise 1 and exercise 2 finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((accumulator, currentIndex) => accumulator + currentIndex, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((accumulator, currentIndex) => accumulator * currentIndex, 1);
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let numbers = [];
+  for(let i = 1; i <= number; i++) {
+    numbers.push(i);
+  }
+  return numbers.reduce((accumulator, currentIndex) => accumulator * currentIndex, 1);
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    const reversedWords = words.replace(/[^a-z0-9]/gmi, "").replace(/\s+/g, "").split('').reverse().join('');
+    return reversedWords.toLowerCase() === words.replace(/[^a-z0-9]/gmi, "").replace(/\s+/g, "").toLowerCase();
 };
 
 // Do not edit below this line
